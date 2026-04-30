62285b2dfca30804e52bcb8d0b290bffb2a3a5ac
Nicholas Coppola <nicholas.coppola67@gmail.com>
2026-04-30 16:34:17 -0400

    Initial Commit

---
+++ MyPlannerTests/MyPlannerTests.swift
@@ -0,0 +1,18 @@
+//
+//  MyPlannerTests.swift
+//  MyPlannerTests
+//
+//  Created by Nicholas Coppola on 4/30/26.
+//
+
+import Testing
+
+struct MyPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
